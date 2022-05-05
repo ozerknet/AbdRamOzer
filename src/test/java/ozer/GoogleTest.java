c58c18cbdcff3f9794f1f85f47b2924a80eb1b74
Ozer <ozerknet@gmail.com>
2022-05-06 00:33:02 +0100

    GoogleTest

---
+++ src/test/java/ozer/GoogleTest.java
@@ -0,0 +1,37 @@
+package ozer;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTest {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://google.com");
+
+
+        WebElement agreeButton = driver.findElement(By.id("L2AGLb"));
+        agreeButton.click();// used to click on any button
+
+        WebElement gmailButtonElement = driver.findElement(By.className("gb_d"));
+        gmailButtonElement.click();
+        //Gmail: Free, Private & Secure Email | Google Workspace
+
+        String expectedTitle = "Gmail: Free, Private & Secure Email | Google Workspace";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println("Title verifiaction PASSED!");
+        } else {
+            System.out.println("Title verification FAIL!!!");
+        }
+    }
+}
