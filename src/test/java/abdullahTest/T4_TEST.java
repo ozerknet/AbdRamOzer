24919d97572f73fafca88316b6ac6d27c31c0a32
AbdullahDanismaz <98493686+AbdullahDanismaz@users.noreply.github.com>
2022-05-12 22:39:47 +0100

    abdullah test

--- src/test/java/abdullahTest/T4_TEST.java
+++ src/test/java/abdullahTest/T4_TEST.java
@@ -16,16 +16,16 @@ public class T4_TEST {
         //Locate header using cssSelector:locate parent element and move down to h2
 
         //a. “Home” link
-        WebElement homeLink_ex1= driver.findElement(By.cssSelector("a[class='nav-link']"));
-        WebElement homeLink_ex2= driver.findElement(By.cssSelector("a.nav-link"));
+       // WebElement homeLink_ex1= driver.findElement(By.cssSelector("a[class='nav-link']"));
+       // WebElement homeLink_ex2= driver.findElement(By.cssSelector("a.nav-link"));
         WebElement homeLink_ex3= driver.findElement(By.cssSelector("a[href='/']"));
 
         //b. “Forgot password” header
-        WebElement forgetPasswordHeader1= driver.findElement(By.cssSelector("div.example>h2"));
+       // WebElement forgetPasswordHeader1= driver.findElement(By.cssSelector("div.example>h2"));
 
-        WebElement forgetPasswordHeader2= driver.findElement(By.xpath("h2[.='Forgot Password'"));
+        WebElement forgetPasswordHeader2= driver.findElement(By.xpath("//h2[.='Forgot Password']"));
 
-        WebElement forgetPasswordHeader3= driver.findElement(By.xpath("h2[text()='Forgot Password'"));
+      // WebElement forgetPasswordHeader3= driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
 
     }
 }
