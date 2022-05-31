5bdba43caed0226d0763ae33a39a7eb5f1dddac4
ramazanyildirim1 <97792743+ramazanyildirim1@users.noreply.github.com>
2022-05-31 11:57:49 +0100

    test3

---
+++ src/test/java/ramazan/test3.java
@@ -0,0 +1,40 @@
+package ramazan;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class test3 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver= cydeo.utilities.WebDriverFactory.getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void window_handling() {
+        driver.get("http://automationexercise.com");
+        WebElement login = driver.findElement(By.xpath("//a[@href='/login']"));
+        login.click();
+        WebElement signVisible = driver.findElement(By.xpath("(//h2)[1]"));
+        System.out.println("signVisible.isDisplayed() = " + signVisible.isDisplayed());
+        WebElement signup = driver.findElement(By.xpath(" //input[@data-qa='login-email'] "));
+        signup.sendKeys("r.y3831@gmail.com");
+        WebElement loginPassword = driver.findElement(By.xpath("//input[@data-qa='login-password'] "));
+        loginPassword.sendKeys("4556518Ry");
+        WebElement loginButton = driver.findElement(By.xpath("//button[@data-qa='login-button']"));
+        loginButton.click();
+        WebElement inCorrect = driver.findElement(By.xpath("//p[@style='color: red;']"));
+        System.out.println("inCorrect.isDisplayed() = " + inCorrect.isDisplayed());
+
+
+
+
+    }
+}
